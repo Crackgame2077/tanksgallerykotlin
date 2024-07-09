d4e8f7b73b887319e8f994749b8ac9943645cbda
Trung <bieuluu53@gmail.com>
2024-07-09 22:27:01 +0700

    first commit

---
+++ app/src/main/java/com/example/tanks/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.example.tanks
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tanks.ui.theme.TanksTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TanksTheme {
+                tanksApp()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun tanksApp() {
+    var currentStep by remember { mutableStateOf(1) }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        when (currentStep) {
+            1 -> TextandImage(
+                textID = R.string.t_72b,
+                imageID = R.drawable.t_72b,
+                descriptionID = R.string.desT72b,
+                onImageClick = { currentStep = 2 }
+            )
+            2 -> TextandImage(
+                textID = R.string.t_90a,
+                imageID = R.drawable.t_90a,
+                descriptionID = R.string.desT90a,
+                onImageClick = { currentStep = 3 }
+            )
+            3 -> TextandImage(
+                textID = R.string.t_90m,
+                imageID = R.drawable.t_90m,
+                descriptionID = R.string.desT90m,
+                onImageClick = { currentStep = 4 }
+            )
+            4 -> TextandImage(
+                textID = R.string.t_72b3,
+                imageID = R.drawable.t_72b3,
+                descriptionID = R.string.desT72b3,
+                onImageClick = { currentStep = 1 }
+            )
+
+        }
+    }
+}
+
+@Composable
+fun TextandImage(
+    textID : Int,
+    imageID : Int,
+    descriptionID : Int,
+    onImageClick : () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = imageID),
+            contentDescription = stringResource(id = descriptionID),
+            modifier = modifier.wrapContentSize()
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(
+            text = stringResource(id = textID),
+            fontSize = 24.sp,
+            modifier = modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            )
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        Row(){
+            Button(
+                onClick = { onImageClick() },
+            ) {
+                Text(text = stringResource(R.string.next), fontSize = 24.sp)
+            }
+            Button(
+                onClick = { onImageClick() },
+            ) {
+                Text(text = stringResource(R.string.previous), fontSize = 24.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TanksTheme {
+        tanksApp()
+    }
+}
